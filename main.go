c279cead59ef5675b7400a00faa0209961e0fc2b
DrVeseli <mark.veselinovic@gmail.com>
2024-12-25 13:35:39 +0100

    map and scoutingĆ

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 )
 
 var worldMap WorldMap
@@ -55,34 +54,6 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		})
 	}
 	return m, nil
-}
-
-var (
-	blackStyle = lipgloss.NewStyle().Background(lipgloss.Color("#000000")).SetString("  ")
-	greenStyle = lipgloss.NewStyle().Background(lipgloss.Color("#00FF00")).SetString("  ")
-	blueStyle  = lipgloss.NewStyle().Background(lipgloss.Color("#0000FF")).SetString("  ")
-	grayStyle  = lipgloss.NewStyle().Background(lipgloss.Color("#808080")).SetString("  ")
-)
-
-func (m model) View() string {
-	var s string
-	for i := 0; i < 100; i++ {
-		for j := 0; j < 100; j++ {
-			index := i*100 + j
-			switch worldMap[index] {
-			case 0:
-				s += blackStyle.String()
-			case 1:
-				s += greenStyle.String()
-			case 2:
-				s += blueStyle.String()
-			case 3:
-				s += grayStyle.String()
-			}
-		}
-		s += "\n"
-	}
-	return s + "\nPress Q to quit\n"
 }
 
 func main() {
